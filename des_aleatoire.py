6057b5a9b9780f1e4dd35738b0b461800e4f6f2f
julienGery <julien.gery@yahoo.com>
2021-10-22 20:17:35 +0200

    updated

--- des_aleatoire.py
+++ des_aleatoire.py
@@ -41,12 +41,11 @@ for row in rows:
             if de !=3:  #dé de 2, 4, 5, 6 cercles
                 if de == 6: #dé de 6 cercles
                     t.goto(nbcarre*(size+size/3), size/2-cercleSize+posY)
-                    for _ in range(2):
-                        t.forward(size/4)
+                    for i in range(1,3):
+                        t.forward(size*i/4)
                         t.begin_fill()
                         t.circle(cercleSize)
                         t.end_fill()
-                        t.forward(size/4)
                 for i in [3,1]: #dé de 2, 4, 5, 6 cercles
                     t.goto(nbcarre*(size+size/3),size-(i/4*size)-cercleSize+posY)
                     t.forward(size*i/4)
@@ -102,10 +101,9 @@ for row in rows:
             if de !=3:  #dé de 2, 4, 5, 6 cercles
                 if de == 6: #dé de 6 cercles
                     t.goto(nbcarre*(size+size/3), size/2-cercleSize+posY)
-                    for _ in range(2):
-                        t.forward(size/4)
+                    for i in range(1,3):
+                        t.forward(size*i/4)
                         myCercle()
-                        t.forward(size/4)
                 for i in [3,1]: #dé de 2, 4, 5, 6 cercles
                     t.goto(nbcarre*(size+size/3),size-(i/4*size)-cercleSize+posY)
                     t.forward(size*i/4)
