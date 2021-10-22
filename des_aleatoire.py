791b9ac41d17610ff988bcaf4f81beb449385168
julienGery <julien.gery@yahoo.com>
2021-10-22 21:29:01 +0200

    upgraded

--- des_aleatoire.py
+++ des_aleatoire.py
@@ -2,8 +2,8 @@ from math import floor
 from turtle import Turtle, exitonclick, setworldcoordinates
 from random import randint
 
-size = 1
-cercleSize = size/8
+size = 1 #valeur sans importance
+cercleSize = size/8 #/6 est la taille max avant les problèmes /10 peut être meilleur
 tirage = int(input('tirage:\t'))
 racineTirage = floor(tirage**(1/2))
 setworldcoordinates(0, 0, (racineTirage+(not(tirage==racineTirage**2)))*(size+size/3), (racineTirage+(not(tirage==racineTirage**2)))*(size+size/3)) #ajustement de la taille de la carte
@@ -16,16 +16,15 @@ tour=0  #varible qui compte le nombre de tour
 for row in rows:
     posY = (size+size/3)*tour   #position sur l'axe y
     tour+=1
-    for nbcarre in range(row):    #création de la ligne et possitionnement de l'axe x
-        t.goto(nbcarre*(size+size/3), posY)
+    for nbcarre in range(row):    #création de la ligne et possitionnement de l'axe x. nbcarre est le numéro du carré en cour sur la rangée
+        t.goto(nbcarre*(size+size/3), posY) #nbcarre*(size+size/3) est le début du carré.
         t.pendown()
         de = randint(1,6)   #numéro du dé
-        print(de)
         for _ in range(4):  #création du carré
             t.forward(size)
             t.left(90)
         t.penup()
-        if de in [1, 3, 5]: #dé de 1, 3, 5 cercles
+        if de in [1, 3, 5]: #dé de 1, 3, 5 cercles équivalent  équivalent: if de == 1 or de == 3 or de == 5. 
             t.goto(nbcarre*(size+size/3)+size/2,(size/2)-(cercleSize)+posY)
             t.begin_fill()
             t.circle(cercleSize)
@@ -39,9 +38,8 @@ for row in rows:
                     t.end_fill()
             if de !=3:  #dé de 2, 4, 5, 6 cercles
                 if de == 6: #dé de 6 cercles
-                    t.goto(nbcarre*(size+size/3), size/2-cercleSize+posY)
-                    for i in range(1,3):
-                        t.forward(size*i/4)
+                    for i in [1, 3]:
+                        t.goto(nbcarre*(size+size/3)+size*i/4, size/2-cercleSize+posY)
                         t.begin_fill()
                         t.circle(cercleSize)
                         t.end_fill()
@@ -53,7 +51,7 @@ for row in rows:
 
 exitonclick()
 
-# using def 
+# using def can be improved. 45 lignes
 """
 from math import floor
 from turtle import Turtle, exitonclick, setworldcoordinates
@@ -87,24 +85,20 @@ for row in rows:
             t.left(90)
         t.penup()
         if de in [1, 3, 5]: #dé de 1, 3, 5 cercles
-            t.forward(size/2)
-            t.sety((size/2)-(cercleSize)+posY)
+            t.goto(nbcarre*(size+size/3)+size/2,(size/2)-(cercleSize)+posY)
             myCercle()
         if de in [2, 3, 4, 5, 6]: #dé de 2, 3, 4, 5, 6 cercles
             if de != 2:
                 for i in [1, 3]:    #dé de 3, 4, 5, 6 cercles
-                    t.goto(nbcarre*(size+size/3), i/4*size-cercleSize+posY)
-                    t.forward(size*i/4)
+                    t.goto(nbcarre*(size+size/3)+size*i/4, i/4*size-cercleSize+posY)
                     myCercle()
             if de !=3:  #dé de 2, 4, 5, 6 cercles
                 if de == 6: #dé de 6 cercles
-                    t.goto(nbcarre*(size+size/3), size/2-cercleSize+posY)
-                    for i in range(1,3):
-                        t.forward(size*i/4)
+                    for i in [1, 3]:
+                        t.goto(nbcarre*(size+size/3)+size*i/4, size/2-cercleSize+posY)
                         myCercle()
                 for i in [3,1]: #dé de 2, 4, 5, 6 cercles
-                    t.goto(nbcarre*(size+size/3),size-(i/4*size)-cercleSize+posY)
-                    t.forward(size*i/4)
+                    t.goto(nbcarre*(size+size/3)+size*i/4,size-(i/4*size)-cercleSize+posY)
                     myCercle()
 
 exitonclick()
