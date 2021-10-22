6d1bfba3440891fa779fa56d1c2e4ae89e85249a
julienGery <julien.gery@yahoo.com>
2021-10-22 19:33:57 +0200

    updated

--- des_aleatoire.py
+++ des_aleatoire.py
@@ -14,14 +14,12 @@ rows = [racineTirage]*racineTirage+[tirage-racineTirage**2]  #création du table
 tour=0  #varible qui compte le nombre de tour 
 
 for row in rows:
-    start = 0   #position sur l'axe x
     posY = (size+size/3)*tour   #position sur l'axe y
     tour+=1
-    for _ in range(row):    #création de la ligne
-        t.goto(start, posY)
+    for nbcarre in range(row):    #création de la ligne et possitionnement de l'axe x
+        t.goto(nbcarre*(size+size/3), posY)
         t.pendown()
         de = randint(1,6)   #numéro du dé
-        end = t.xcor()+size #mémorisation de la fin/début
         for _ in range(4):  #création du carré
             t.forward(size)
             t.left(90)
@@ -32,17 +30,17 @@ for row in rows:
             t.begin_fill()
             t.circle(cercleSize)
             t.end_fill()
-        if de in [2, 5, 4, 3, 6]: #dé de 2, 4, 5, 6 cercles
+        if de in [2, 3, 4, 5, 6]: #dé de 2, 3, 4, 5, 6 cercles
             if de != 2:
                 for i in [1, 3]:    #dé de 3, 4, 5, 6 cercles
-                    t.goto(start, i/4*size-cercleSize+posY)
+                    t.goto(nbcarre*(size+size/3), i/4*size-cercleSize+posY)
                     t.forward(size*i/4)
                     t.begin_fill()
                     t.circle(cercleSize)
                     t.end_fill()
             if de !=3:  #dé de 2, 4, 5, 6 cercles
                 if de == 6: #dé de 6 cercles
-                    t.goto(start, size/2-cercleSize+posY)
+                    t.goto(nbcarre*(size+size/3), size/2-cercleSize+posY)
                     for _ in range(2):
                         t.forward(size/4)
                         t.begin_fill()
@@ -50,12 +48,10 @@ for row in rows:
                         t.end_fill()
                         t.forward(size/4)
                 for i in [3,1]: #dé de 2, 4, 5, 6 cercles
-                    t.goto(start,size-(i/4*size)-cercleSize+posY)
+                    t.goto(nbcarre*(size+size/3),size-(i/4*size)-cercleSize+posY)
                     t.forward(size*i/4)
                     t.begin_fill()
                     t.circle(cercleSize)
                     t.end_fill()
 
-        start = end+size/3
-        
 exitonclick()
