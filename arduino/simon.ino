49d222f53990f743b1f30766cec8627c50085810
julienGery <julien.gery@yahoo.com>
2022-02-03 22:40:47 +0100

    f

--- arduino/simon.ino
+++ arduino/simon.ino
@@ -9,17 +9,17 @@ const int button[] = {GREENBUTTON, ORANGEBUTTON, REDBUTTON};
 const int led[] = {GREENLED, ORANGELED, REDLED};
 int values[50];
 int inputs[3];
-int previusPin = button[0]; //defaut value
-long Ttime;
+byte previusPin = button[0]; //defaut value
 
-void checkPrevius(int &pin){
-  Ttime = millis();
-  do{
-    if(digitalRead(pin) == HIGH){
-      Ttime = millis();
+void checkPrevius(byte &pin){
+  int boucle = 0;
+  while(boucle<10){
+    boucle++;
+    if(digitalRead(pin)){
+      boucle = 0;
     }
     delay(1);
-  }while(millis()<=Ttime+10);
+  }
 }
 
 int getData(){
@@ -45,14 +45,13 @@ void setup() {
 }
 
 void dis(int size){
+  randomSeed(analogRead(A0));
   for(int i = 0; i<=size; i++){
     digitalWrite(led[values[i]], HIGH);
-    // Serial.print(values[i]);
     delay(250);
     digitalWrite(led[values[i]], LOW);
     delay(250);
   }
-  // Serial.println();
 }
 
 
@@ -62,11 +61,14 @@ void loop() {
   values[i] = random(0,3);
   dis(i);
   for(int j = 0; j<=i; j++){
-    // Serial.println(j);
-    int tmp = getData();
-    Serial.println(tmp);
-    if(values[j] != tmp){
-      Serial.println("bites");
+    if(values[j] != getData()){
+      Serial.println("perdu");
+      for(auto &k : led){
+        digitalWrite(k, HIGH);
+      }
+      while(true){
+        delay(5);
+      }
     }
   }
   delay(1000);
