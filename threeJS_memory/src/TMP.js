6b77c64ed6f293e8b409d1fc96bffbfc388ecddd
julienGery <julien.gery@yahoo.com>
2022-04-23 02:08:16 +0200

    explosion

--- threeJS_memory/src/TMP.js
+++ threeJS_memory/src/TMP.js
@@ -1,276 +1,19 @@
 import './style.css'
 import * as THREE from 'three'
+import Stats from 'stats.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-const axios = require('axios')
 
-
-const loader = new THREE.TextureLoader();
-const texture2 = loader.load('https://raw.githubusercontent.com/JulienGery/nsi/main/threeJS_memory/static/tmp.jpg') //front
-const mouse = new THREE.Vector2();
-const raycaster = new THREE.Raycaster();
-const dim = new THREE.Vector3(1, 1, .001)
-const nb_card = parseInt(prompt("nombre de pair"))
-const racineNb = Math.floor(Math.sqrt(nb_card) * 2)
-const Zpos = Math.ceil(nb_card * 2 / racineNb)
-const allCard = []
-const textureDL = []
-
-
-class card {
-
-    constructor(dim, pos, texture, name) {
-        this.x = dim.x
-        this.y = dim.y
-        this.z = dim.z
-        this.px = pos.x
-        this.py = pos.y
-        this.pz = pos.z
-        this.texture = texture;
-        this.name = name;
-        this.geometry = new THREE.BoxBufferGeometry(this.x, this.y, this.z, 2, 2, 2);
-        this.card = this.makeCard();
-        this.card.name = this.name
-        this.uuid = this.card.uuid;
-
-    }
-
-    makeCard() {
-
-        return new THREE.Mesh(this.geometry, [
-            new THREE.MeshStandardMaterial({ color: "#000000", side: THREE.DoubleSide }),
-            new THREE.MeshStandardMaterial({ color: "#000000", side: THREE.DoubleSide }),
-            new THREE.MeshStandardMaterial({ color: "#000000", side: THREE.DoubleSide }),
-            new THREE.MeshStandardMaterial({ color: "#000000", side: THREE.DoubleSide }),
-            new THREE.MeshStandardMaterial({ color: "#ffffff", side: THREE.DoubleSide, map: texture2 }), //front
-            new THREE.MeshStandardMaterial({ color: "#ffffff", side: THREE.DoubleSide, map: this.texture }), //back
-        ])
-
-    }
-
-    show() {
-        scene.add(this.card)
-    }
-
-    setPlace(x, y, z) {
-        this.px = x
-        this.py = y
-        this.pz = z
-        this.card.position.set(x, y, z)
-    }
-
-
-}
-
-
-const shuffleArray = array => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-
-
-
-async function populate_texture(url) {
-    loader.loadAsync(url).then((rep) => {
-        textureDL.push(rep)
-        if (textureDL.length == nb_card) {
-
-            for (let i = 0; i < textureDL.length; i++) {
-                for (let j = 0; j < 2; j++) {
-                    allCard.push(new card(dim, new THREE.Vector3(0, 0, 0), textureDL[i], i))
-                }
-            }
-
-
-            shuffleArray(allCard)
-            
-
-            for (let i = 0; i < racineNb; i++) {
-                for (let j = 0; j < Zpos && i * Zpos + j < nb_card * 2; j++) {
-                    allCard[i * Zpos + j].setPlace(i * 1.2, j * 1.2, 0)
-                    allCard[i * Zpos + j].show()
-                }
-            }
-
-        }
-    }).catch((err) => {
-        populate_texture(url)
-    })
-}
-
-
-
-
-
-
-async function foo() {
-    axios.get("https://picsum.photos/v2/list?page=" + Math.floor(Math.random() * 10) + "&limit=" + nb_card).then((response) => {
-        const data = response.data
-        for (let i = 0; i < data.length; i++) {
-            populate_texture(data[i].download_url)
-        }
-    })
-}
-
-foo()
-
-// const bite = await axios.get("https://picsum.photos/v2/list?page="+Math.floor(Math.random()*10)+"&limit=" + nb_card).then((response) => {
-
-//     const data = response.data
-//     return data
-
-
-//     // for(let i = 0; i<data.length; i++){
-//     //     console.log(i)
-//     //     axios.get(data[i].download_url).then((image) => {
-//     //         textureDL.push(image.data)
-//     //     }).catch((error) => {
-//     //         console.log(error)
-//     //     })
-//     // }
-//     // console.log(textureDL)
-//     // populateCard(response.data)
-//     // shuffleArray(allCard)
-
-
-//     // for (let i = 0; i < racineNb; i++) {
-//     //     for (let j = 0; j < Zpos && i * Zpos + j < nb_card * 2; j++) {
-//     //         allCard[i * Zpos + j].setPlace(i * 1.2, j * 1.2, 0)
-//     //     }
-//     // }
-
-
-
-//     // for(let i = 0; i<allCard.length; i++){
-//     //     allCard[i].show()
-//     // }
-// })
-
-// for(let i = 0; i<bite.length; i++){
-//     console.log(bite[i])
-// }
-
-// Debug
-const gui = new dat.GUI()
-
-// Canvas
 const canvas = document.querySelector('canvas.webgl')
+const stats = new Stats();
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 
-// Scene
-const scene = new THREE.Scene()
-scene.background = new THREE.Color(0xffffff)
+const pointLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(pointLight);
 
+stats.showPanel(0);
+document.body.appendChild(stats.dom);
 
-
-//compariason des nom des cartes 
-
-
-
-const rotate = (intersects, coef, end, start = 0) => {
-
-    const intersect = intersects
-    const coef2 = coef
-    let clock2 = new THREE.Clock()
-
-    const jpp = () => {
-        const elapsedTime = clock2.getElapsedTime()
-        intersect.rotation.y = 10 * elapsedTime * coef2 + start
-
-        renderer.render(scene, camera)
-        if (elapsedTime < 0.31415926535) {
-            window.requestAnimationFrame(jpp)
-        }
-        else {
-            intersect.rotation.y = end
-        }
-    }
-
-    window.requestAnimationFrame(jpp)
-
-}
-
-let haveRotate = []
-
-const compare = (intersect, array) => {
-    if (array.length == 0) {
-        return true
-    }
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].uuid == intersect.object.uuid) {
-            return false
-        }
-    }
-    return true
-}
-
-function onMouseClick(event) {
-    if (haveRotate.length == 2) {
-        if (haveRotate[0].name == haveRotate[1].name) {
-            for (let i = 0; i < allCard.length; i++) {
-                if (allCard[i].name === haveRotate[0].name) {
-                    allCard.pop(i)
-                    allCard.pop(i)
-                    break
-                }
-            }
-
-            for (let i = 0; i < haveRotate.length; i++) {
-                scene.remove(haveRotate[i])
-            }
-        }
-
-        else for (let i = 0; i < haveRotate.length; i++) {
-            rotate(haveRotate[i], -1, 0, Math.PI)
-        }
-
-        haveRotate = []
-    }
-
-    const intersects = pickCard()
-
-    if (intersects.length == 2) {
-        if (compare(intersects[0], haveRotate)) {
-            haveRotate.push(intersects[0].object);
-            rotate(intersects[0].object, 1, Math.PI)
-        }
-    }
-
-}
-
-
-function onPointerMove(event) {
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-}
-
-const pickCard = () => {
-    raycaster.setFromCamera(mouse, camera);
-    return raycaster.intersectObjects(scene.children);
-}
-
-const mouseHover = () => {
-    const intersects = pickCard()
-
-    if (intersects.length == 2) {
-        for (let i = 0; i < intersects.length; i++) {
-            intersects[i].object.material[4].color = new THREE.Color(0x000000)
-        }
-    }
-}
-
-// Lights
-
-const pointLight = new THREE.AmbientLight(0xffffff, 1)
-
-scene.add(pointLight)
-
-/**
- * Sizes
- */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -290,24 +33,6 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-
-
-
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = Zpos / 2
-camera.position.y = racineNb / 2
-camera.position.z = 20
-camera.lookAt(new THREE.Vector3(Zpos / 2, racineNb / 2, 0))
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
@@ -316,21 +41,88 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 0, 20)
+const controls = new OrbitControls(camera, canvas)
+scene.add(camera)
 
-/**
- * Animate
- */
+const nbObject = 5000
 
-
-scene.updateMatrixWorld();
-
-const tick = () => {
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(tick)
+function randomUnitVector() {
+    const vec = new THREE.Vector3(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1).normalize();
+    return vec;
 }
 
-window.addEventListener('click', onMouseClick);
-window.addEventListener('pointermove', onPointerMove);
+const color = new THREE.Color();
 
+class explosion {
+    constructor(x, y) {
+        this.clock = new THREE.Clock()
+        this.x = x
+        this.y = y
+        this.speeds = []
+        this.createGeometry()
+        this.points = new THREE.Points(this.geometry, new THREE.PointsMaterial({ vertexColors: true, size: .15 }))
+        scene.add(this.points)
+        this.position = this.geometry.attributes.position
+    }
+
+    createGeometry() {
+        this.geometry = new THREE.BufferGeometry();
+        const vertices = []
+        const colors = []
+        for (let i = 0; i < nbObject; i++) {
+            color.setHSL(1, Math.random(), Math.random()*2);
+            colors.push(color.r, color.g, color.b);
+            const vec = randomUnitVector()
+            vec.multiplyScalar(.1)
+            vertices.push(vec.x + this.x, vec.y + this.y, vec.z)
+            this.speeds.push(Math.random())
+        }
+        this.geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3))
+        this.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
+    }
+
+    update() {
+        const elapsedTime = this.clock.getElapsedTime()
+        for (let i = 0; i < nbObject; i++) {
+            const speed = this.speeds[i]
+            const x = this.position.getX(i)
+            const y = this.position.getY(i)
+            const z = this.position.getZ(i)
+            this.position.setXYZ(i, (x - this.x) * speed * elapsedTime + x, (y - this.y) * speed * elapsedTime + y, z * speed * elapsedTime + z)
+        }
+        this.position.needsUpdate = true;
+    }
+    remove() {
+        scene.remove(this.points)
+    }
+}
+
+let tmp = []
+tmp.push(new explosion(0, 0))
+
+const caca = () => {
+
+    const vec = randomUnitVector()
+    tmp.push(new explosion(vec.x * Math.random() * 10, vec.y * 10 * Math.random()))
+}
+
+console.log(tmp[0])
+
+window.addEventListener('click', caca)
+
+function tick() {
+    stats.begin();
+    for (let i = 0; i < tmp.length; i++) {
+        tmp[i].update()
+    }
+
+    renderer.render(scene, camera);
+
+    stats.end();
+
+    window.requestAnimationFrame(tick);
+}
 
 tick()
--- threeJS_memory/src/script.js
+++ threeJS_memory/src/script.js
@@ -1,9 +1,21 @@
 import './style.css'
 import * as THREE from 'three'
-import Stats from 'stats.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import Stats from 'stats.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+const axios = require('axios')
 
+const nombreParticules = 2000;
+const gltfLoader = new GLTFLoader();
+const loader = new THREE.TextureLoader();
+const texture2 = loader.load('https://raw.githubusercontent.com/JulienGery/nsi/main/threeJS_memory/static/tmp.jpg') //front
 const canvas = document.querySelector('canvas.webgl')
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+const dim = new THREE.Vector3(1, 1, .001);
+const gui = new dat.GUI();
+const nb_card = parseInt(prompt("nombre de paires"));
 const stats = new Stats();
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
@@ -42,20 +54,170 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0, 20)
-const controls = new OrbitControls(camera, canvas)
+
 scene.add(camera)
 
-const nbObject = 5000
+
+class Card {
+
+    constructor(pos, texture, name, gltf) {
+        this.pos = pos //vector 3
+        this.gltf = gltf.clone(true); //clone the gltf which is the card
+        this.gltf.children[0].children[0].material = new THREE.MeshBasicMaterial({ color: "#ffffff", map: texture }) //setting back texture 
+        this.gltf.children[0].children[1].material = new THREE.MeshBasicMaterial({ color: "#ffffff", map: texture2 })//setting front texture 
+        // this.gltf.children[0].children[2].material = new THREE.MeshBasicMaterial({ color: "#000000"}) //side
+        this.name = name; //name which define pairs
+        this.uuid = this.gltf.uuid //get the uuid of the shape for convenience
+    }
+
+    show() {
+        scene.add(this.gltf)//add gltf to the scene
+    }
+
+
+    setPlace(vector) {
+        this.pos = vector
+        this.gltf.position.set(vector.x, vector.y, vector.z)
+    }
+
+    rotate(start, end, coef) {
+        const clock = new THREE.Clock();
+
+        const actualRotate = () => {
+            //TODO use quaternion (and see why does it break)
+            const elapsedTime = clock.getElapsedTime();
+            this.gltf.rotation.y = 10 * elapsedTime * coef + start;
+
+            // const q = start.slerp(end, elapsedTime*.01)
+            // this.card.setRotationFromQuaternion(q)
+
+            if (elapsedTime < 0.31415926535) {
+                window.requestAnimationFrame(actualRotate)
+            }
+            else {
+                // this.card.setRotationFromQuaternion(end)
+                this.gltf.rotation.y = end;
+            }
+        }
+
+        window.requestAnimationFrame(actualRotate);
+
+    }
+
+    async moveTo(from, to, time = 1) {
+
+        const clock = new THREE.Clock();
+
+        const actualMouveTo = async () => {
+            const elapsedTime = clock.getElapsedTime();
+
+            const lerpPos = from.lerp(to, elapsedTime / (10 * time))
+            // this.gltf.position.set(lerpPos.x, lerpPos.y, lerpPos.z)
+            this.setPlace(lerpPos)
+            if (elapsedTime <= time+.02) {
+                window.requestAnimationFrame(actualMouveTo);
+            } else {
+                this.setPlace(to)
+            }
+        }
+        window.requestAnimationFrame(actualMouveTo)
+
+    }
+
+
+    remove() {
+        scene.remove(this.gltf);
+    }
+
+}
+
+
+class Game {
+
+    constructor(numberCard) {
+        this.numberCard = numberCard;
+        this.allCard = [];
+        this.sqrtNumberCard = Math.floor(Math.sqrt(nb_card) * 2);
+        this.Ypos = Math.ceil(this.numberCard * 2 / this.sqrtNumberCard);
+        this.offSet = new THREE.Vector3((this.sqrtNumberCard - 1) * 2.3 / 2, (this.Ypos - 1) * 3.7 / 2, 0)
+        this.CreateCard();
+    }
+
+    CreateCard() {
+        gltfLoader.load('https://raw.githubusercontent.com/JulienGery/nsi/main/threeJS_memory/static/carte.gltf', (gltf) => {
+            this.getImage(gltf)//doawnload gltf of the card and passing it to constructor. it's kinda ugly but...
+        }, (progess) => {
+
+        }, (error) => {
+            this.CreateCard()//recall on error
+        })
+    }
+
+    async getImage(gltf) {
+        await axios.get("https://picsum.photos/v2/list?page=" + Math.floor(Math.random() * 1000 / this.numberCard) + "&limit=" + nb_card).then((response) => {
+            const data = response.data;//getting image urls
+            for (let i = 0; i < data.length; i++) {
+                this.downloadTexture(data[i].download_url, gltf.scene)//TODO allow user to enter url(s)
+            }
+        }).catch((err) => {
+            this.getImage(gltf)
+        })
+
+
+    }
+
+    async downloadTexture(URL, gltf) {
+        loader.loadAsync(URL).then((rep) => {
+            for (let i = 0; i < 2; i++) {
+                this.allCard.push(new Card(new THREE.Vector3(0, 0, 0), rep, this.allCard.length - i, gltf))//creating card
+            }
+            if (this.allCard.length == this.numberCard * 2) {
+                this.shuffleArray(this.allCard); //kinda ugly again but it work so...
+                this.placeCard();
+            }
+        }).catch((err) => {
+            console.log(err);
+            this.downloadTexture(URL, gltf); //recall on error
+        })
+    }
+
+
+    shuffleArray() {
+        for (let i = this.allCard.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.allCard[i];
+            this.allCard[i] = this.allCard[j];
+            this.allCard[j] = temp;
+        }
+    }
+
+    placeCard() {
+        for (let i = 0; i < this.allCard.length; i++) {
+            this.allCard[i].show()
+        }
+    }
+
+    spread() {
+        for (let i = 0; i < this.sqrtNumberCard; i++) {
+            for (let j = 0; j < this.Ypos && i * this.Ypos + j < this.numberCard * 2; j++) {
+                this.allCard[i * this.Ypos + j].moveTo(this.allCard[i * this.Ypos + j].pos, new THREE.Vector3(i * 2.3, j * 3.7, 0).sub(this.offSet));
+            }
+        }
+    }
+
+    //actual game
+    //still nothing
+
+}
+
 
 function randomUnitVector() {
     const vec = new THREE.Vector3(Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1).normalize();
     return vec;
 }
 
-const color = new THREE.Color();
 
-class explosion {
+class Explosion {
     constructor(x, y) {
         this.clock = new THREE.Clock()
         this.x = x
@@ -70,9 +232,10 @@ class explosion {
     createGeometry() {
         this.geometry = new THREE.BufferGeometry();
         const vertices = []
+        const color = new THREE.Color()
         const colors = []
-        for (let i = 0; i < nbObject; i++) {
-            color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+        for (let i = 0; i < nombreParticules; i++) {
+            color.setHSL(1, Math.random(), Math.random()*2);
             colors.push(color.r, color.g, color.b);
             const vec = randomUnitVector()
             vec.multiplyScalar(.1)
@@ -83,9 +246,9 @@ class explosion {
         this.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3))
     }
 
-    update() {
+    async update() {
         const elapsedTime = this.clock.getElapsedTime()
-        for (let i = 0; i < nbObject; i++) {
+        for (let i = 0; i < nombreParticules; i++) {
             const speed = this.speeds[i]
             const x = this.position.getX(i)
             const y = this.position.getY(i)
@@ -99,27 +262,150 @@ class explosion {
     }
 }
 
-let tmp = []
-tmp.push(new explosion(0, 0))
 
-const caca = () => {
+const game = new Game(nb_card)
 
-    const vec = randomUnitVector()
-    tmp.push(new explosion(vec.x * Math.random() * 10, vec.y * 10 * Math.random()))
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 20
+
+let haveRotate = []
+
+function onPointerMove(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 }
 
-console.log(tmp[0])
+const ponterMoveAspread = (event) => {
+    onPointerMove(event)
+    onMouseOver()
+}
 
-window.addEventListener('click', caca)
+function pickCard() {
+
+    raycaster.setFromCamera(mouse, camera);
+    return raycaster.intersectObjects(scene.children, true);
+
+}
+
+function onMouseClick(event) {
+
+    // const qFront = new THREE.Quaternion(0, 0, 0, 1);
+    // const qBack = new THREE.Quaternion(0, 1, 0, 0);
+
+    if (haveRotate.length == 2) {
+        if (haveRotate[0] < haveRotate[1]) {
+            haveRotate.reverse();
+        }
+        if (game.allCard[haveRotate[0]].name == game.allCard[haveRotate[1]].name) {
+            for (let i = 0; i < haveRotate.length; i++) {
+                const PopingCard = game.allCard.splice(haveRotate[i], 1)[0];
+                explosion.push(new Explosion(PopingCard.pos.x, PopingCard.pos.y))
+                if (cardUnder.includes(haveRotate[i])) {
+                    cardUnder = []
+                }
+                PopingCard.remove();
+            }
+        }
+        else {
+            for (let i = 0; i < haveRotate.length; i++) {
+                game.allCard[haveRotate[i]].rotate(Math.PI, 0, -1);
+            }
+        }
+        haveRotate = [];
+    }
+
+    if (cardUnder.length >= 1) {
+        const cardIndex = cardUnder[0]
+        if (!haveRotate.includes(cardIndex, 0)) {
+            game.allCard[cardIndex].rotate(0, Math.PI, 1);
+            haveRotate.push(cardIndex);
+            moveDown()
+        }
+    }
+
+
+    
+    // const intersects = pickCard()
+
+    // if (intersects.length == 1) {
+    //     for (let i = 0; i < game.allCard.length; i++) {
+    //         if (game.allCard[i].uuid == intersects[0].object.parent.parent.uuid) {
+    //             if (!haveRotate.includes(i, 0)) {
+    //                 game.allCard[i].rotate(0, Math.PI, 1);
+    //                 haveRotate.push(i);
+    //                 moveDown()
+    //                 break
+    //             }
+    //         }
+    //     }
+    // }
+
+
+}
+
+let cardUnder = []
+let explosion = []
+
+const moveUp = (i) => {
+    const pos = game.allCard[i].pos.clone()
+    game.allCard[i].moveTo(pos, new THREE.Vector3(pos.x, pos.y, 1), .3)
+}
+
+const moveDown = (i = 0) => {
+    const pos = game.allCard[cardUnder[i]].pos.clone()
+    game.allCard[cardUnder[i]].moveTo(pos, new THREE.Vector3(pos.x, pos.y, 0), .3)
+}
+
+const onMouseOver = () => {
+    const intersects = pickCard();
+    if (intersects.length == 1) {
+        for (let i = 0; i < game.allCard.length; i++) {
+            //getting index of the card under the mouse in allCard and moving it up and moving the older card down
+            if (game.allCard[i].uuid == intersects[0].object.parent.parent.uuid) {
+                if (!cardUnder.includes(i, 0) && !haveRotate.includes(i, 0)) {
+                    if (cardUnder.length > 0) {
+                        moveDown()
+                        cardUnder = []
+                    }
+                    cardUnder.push(i)
+                    moveUp(i)
+                }
+            }
+        }
+    } else if (cardUnder.length > 0) {
+        //moving the older card down
+        moveDown()
+        cardUnder = []
+    }
+}
+
+
+const beforeSpread = (event) => {
+    game.spread()
+    window.removeEventListener('click', beforeSpread)
+    window.removeEventListener('pointermove', onPointerMove)
+
+    const addListner = () => {
+        const controls = new OrbitControls(camera, canvas)
+        // controls.enableDamping = true    
+        window.addEventListener('click', onMouseClick)
+        window.addEventListener('pointermove', ponterMoveAspread)
+    }
+    setTimeout(addListner, 1200);
+}
+
+window.addEventListener('click', beforeSpread);
+window.addEventListener('pointermove', onPointerMove);
 
 function tick() {
     stats.begin();
-    for (let i = 0; i < tmp.length; i++) {
-        tmp[i].update()
+
+    for(let i = 0; i<explosion.length; i++){
+        explosion[i].update()
     }
 
     renderer.render(scene, camera);
-
     stats.end();
 
     window.requestAnimationFrame(tick);
