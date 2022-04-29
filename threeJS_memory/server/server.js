8e492a5f8698b6f67e8c2f16b735d6f61b9bcd5c
julienGery <julien.gery@yahoo.com>
2022-04-29 19:23:08 +0200

    server

--- threeJS_memory/server/server.js
+++ threeJS_memory/server/server.js
@@ -9,20 +9,40 @@ const io = require("socket.io")(3000, {
     }
 })
 
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
 const initCards = (room) => {
-    const Rcards = rooms[room].cards
+    const roomCards = rooms[room].cards
     const cards = []
-    for (let i = 0; i < Rcards.length; i++) {
+    for (let i = 0; i < roomCards.length; i++) {
         for (let j = 0; j < 2; j++) {
             cards.push({
-                "name": cards.length - i,
-                "textureURL": Rcards.cards[i]
+                "name": cards.length - j,
+                "textureURL": roomCards[i]
             })
         }
     }
+    shuffleArray(cards)
     return cards
 }
 
+// const getCards = (room) => {
+//     return rooms[room].cards
+// }
+
+const setPlayersNotReady = (room) => {
+    const players = rooms[room].players
+    players.forEach(id => {
+        users[id].ready = false
+    });
+}
 
 const getRoomStatus = (room) => {
     const players = []
@@ -60,14 +80,15 @@ io.on('connect', socket => {
         users[socket.id]
         if (!rooms[room]) {
             rooms[room] = {
+                "started": false,
                 "playerTurn": 0,
                 "players": [],
-                "cards": []
+                "cards": ["https://raw.githubusercontent.com/JulienGery/nsi/main/threeJS_memory/static/snkellefaitpeur.png"]
             }
         }
 
         rooms[room].players.push(socket.id)
-        cardsURL.map((url) => rooms[room].cards.push(url))
+        // cardsURL.map((url) => rooms[room].cards.push(url))
         const roomStatus = getRoomStatus(room)
         socket.to(room).emit('update-room', roomStatus)
         // console.log(io.sockets.adapter.rooms.get(room))
@@ -75,42 +96,53 @@ io.on('connect', socket => {
     })
 
 
-    socket.on('action', (action, cardIndex, room) => {
+    socket.on('action', (action, cardIndex) => {
+        const room = users[socket.id].room
         switch (action) {
             case 'turn-card':
                 console.log('turn card\t' + cardIndex)
-                socket.to(room).emit('turn-card', cardIndex);
+                socket.to(room).emit('action', action, cardIndex);
                 break;
 
             case 'move-up':
                 console.log('moving-up\t' + cardIndex)
-                socket.to(room).emit('move-up', cardIndex);
+                socket.to(room).emit('action', action, cardIndex);
                 break;
 
             case 'move-down':
-                console.log('moving down')
-                socket.to(room).emit('move-down', cardIndex);
+                console.log('moving down\t' + cardIndex)
+                socket.to(room).emit('action', action, cardIndex);
                 break;
 
             case 'pair-found':
                 //TODO add point
                 console.log('pair found\t' + cardIndex)
-                socket.to(room).emit('pair-found', cardIndex);
+                socket.to(room).emit('action', action, cardIndex);
                 break;
             case 'turnback-card':
                 console.log('turnback card\t' + cardIndex)
-                socket.to(room).emit('turnback-card', cardIndex)
+                socket.to(room).emit('action', action, cardIndex)
                 break;
 
         }
     })
 
-    socket.on('send-cards', (cards, room) => {
-        socket.to(room).emit('receive-cards', cards)
+    socket.on('submit-cards', cards => {
+        const room = users[socket.id].room
+        rooms[room].cards.push(cards)
+        socket.to(room).emit('update-cards', rooms[room].cards)
+        cb(rooms[room].cards)
     })
 
-    socket.on('next-player', (room) => {
+    // socket.on('send-cards', () => {
+    //     const room = users[socket.id].room
+    //     const cards = initCards(users[socket.id].room)
+    //     socket.to(room).emit('receive-cards', cards)
+    // })
 
+    socket.on('next-player', () => {
+
+        const room = users[socket.id].room
         console.log('next-player')
 
         rooms[room].playerTurn = (rooms[room].playerTurn + 1) % rooms[room].players.length
@@ -118,22 +150,46 @@ io.on('connect', socket => {
 
         // console.log(rooms[room].players[playerTurn].name)
         // console.log(playerTurn)
-        socket.to(rooms[room].players[playerTurn].socketID).emit('next-player')
+        socket.to(rooms[room].players[playerTurn]).emit('next-player')
 
     })
 
-    socket.on('ready', (cb) => {
+    socket.on('ready', () => {
 
-        console.log('ready')
+        console.log(`${users[socket.id].name} is ready !!!`)
         users[socket.id].ready = true
         const room = users[socket.id].room
-        socket.to(room).emit('update-room', getRoomStatus(room))
+        io.to(room).emit('update-room', getRoomStatus(room))
 
         if (arePlayerReady(room)) {
-            socket.to(room).emit('start-game')
-            cb(true)
+            if (rooms[room].started) {
+                console.log('start game')
+                io.to(room).emit('start-game')
+                setTimeout(() => {
+                    io.to(rooms[room].players[Math.floor(rooms[room].players.length * Math.random())]).emit('next-player')
+                }, 1200);
+            } else {
+                console.log('send cards')
+
+                rooms[room].started = true
+                setPlayersNotReady(room)
+                io.to(room).emit('update-room', getRoomStatus(room))
+
+                const cards = initCards(room)
+                io.to(room).emit('receive-cards', cards)
+            }
         }
-        cb(false)
+        
+    })
+
+    socket.on('disconnect', () => {
+        const user = users[socket.id]
+        const room = user.room
+        console.log(`${user.name} has left`)
+        rooms[room].players.splice(rooms[room].players.indexOf(socket.id), 1)
+        socket.to(room).emit('update-room', getRoomStatus(room))
+        socket.leave(room)
+        delete users[user]
     })
 })
 
--- threeJS_memory/src/script.js
+++ threeJS_memory/src/script.js
@@ -2,24 +2,24 @@ import { io } from 'socket.io-client'
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 import Stats from 'stats.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { Explosion } from './explosion.js'
 import { Card } from './card.js'
-const axios = require('axios')
 
 const name = prompt('name')
-const room = prompt("room")
-const socket = io("http://localhost:3000")
+const room = prompt('room')
 const gltfLoader = new GLTFLoader();
 const loader = new THREE.TextureLoader();
 const canvas = document.querySelector('canvas.webgl')
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-const nb_card = parseInt(prompt("nombre de paires"));
+const gui = new dat.GUI();
 const stats = new Stats();
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
+const socket = io("http://192.168.1.25:3000")
 
 const pointLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(pointLight);
@@ -60,76 +60,39 @@ scene.add(camera)
 
 class Game {
 
-  constructor(numberCard) {
-    this.numberCard = numberCard;
-    this.allCard = [];
-    this.sqrtNumberCard = Math.floor(Math.sqrt(this.numberCard) * 2);
-    this.Ypos = Math.ceil(this.numberCard * 2 / this.sqrtNumberCard);
+  constructor(cards) {
+
+    this.numberCard = cards.length;
+    this.allCard = [].fill(0, 0, this.numberCard);
+    this.sqrtNumberCard = Math.floor(Math.sqrt(this.numberCard / 2) * 2);
+    this.Ypos = Math.ceil(this.numberCard / this.sqrtNumberCard);
     this.offSet = new THREE.Vector3((this.sqrtNumberCard - 1) * 2.3 / 2, (this.Ypos - 1) * 3.7 / 2, 0)
-    this.CreateCard();
+    this.CreateCard(cards);
+
   }
 
-  CreateCard() {
+  CreateCard(cards) {
     gltfLoader.load('https://raw.githubusercontent.com/JulienGery/nsi/main/threeJS_memory/static/carte.gltf', (gltf) => {
-      this.getImage(gltf)//doawnload gltf of the card and passing it to constructor. it's kinda ugly but...
+      for (let i = 0; i < cards.length; i++) {
+        this.downloadTexture(cards[i].textureURL, cards[i].name, i, gltf.scene)//doawnload gltf of the card and passing it to constructor. it's kinda ugly but...
+      }
     }, (progess) => {
-
     }, (error) => {
-      this.CreateCard()//recall on error
+      this.CreateCard(cards)//recall on error
     })
   }
 
-  async getImage(gltf) {
-    await axios.get("https://picsum.photos/v2/list?page=" + Math.floor(Math.random() * 1000 / this.numberCard) + "&limit=" + nb_card).then((response) => {
-      const data = response.data;//getting image urls
-      for (let i = 0; i < data.length; i++) {
-        this.downloadTexture(data[i].download_url, gltf.scene)//TODO allow user to enter url(s)
-      }
-    }).catch((err) => {
-      this.getImage(gltf)
-    })
-
-
-  }
-
-  async downloadTexture(URL, gltf) {
+  async downloadTexture(URL, name, index, gltf) {
     loader.loadAsync(URL).then((rep) => {
-      for (let i = 0; i < 2; i++) {
-        this.allCard.push(new Card(new THREE.Vector3(0, 0, 0), rep, this.allCard.length - i, gltf, URL, scene))//creating card
-      }
-      if (this.allCard.length == this.numberCard * 2) {
-        for (let i = 0; i < 22; i++) {
-          this.shuffleArray(this.allCard);
-        }
-        this.sendCards();
-        this.placeCard(); //kinda ugly again but it work so...
+      this.allCard[index] = new Card(new THREE.Vector3(0, 0, 0), rep, name, gltf, URL, scene)//creating card
+      if (!this.allCard.includes(undefined)) {
+        //kinda ugly again but it work so...
+        this.placeCard();
       }
     }).catch((err) => {
       console.log(err);
-      this.downloadTexture(URL, gltf); //recall on error
+      //this.downloadTexture(URL, gltf); //recall on error
     })
-  }
-
-
-  shuffleArray() {
-    for (let i = this.allCard.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = this.allCard[i];
-      this.allCard[i] = this.allCard[j];
-      this.allCard[j] = temp;
-    }
-  }
-
-  async sendCards() {
-    let cards = []
-    for (let i = 0; i < this.allCard.length; i++) {
-      cards.push({
-        "name": this.allCard[i].name,
-        "textureURL": this.allCard[i].textureURL,
-      })
-    }
-    socket.emit('send-cards', cards, room)
-    cards = []
   }
 
   placeCard() {
@@ -140,23 +103,19 @@ class Game {
 
   spread() {
     for (let i = 0; i < this.sqrtNumberCard; i++) {
-      for (let j = 0; j < this.Ypos && i * this.Ypos + j < this.numberCard * 2; j++) {
-        this.allCard[i * this.Ypos + j].moveTo(this.allCard[i * this.Ypos + j].pos, new THREE.Vector3(i * 2.3, j * 3.7, 0).sub(this.offSet));
+      for (let j = 0; j < this.Ypos && i * this.Ypos + j < this.numberCard; j++) {
+        this.allCard[i * this.Ypos + j].moveTo(this.allCard[i * this.Ypos + j].pos, new THREE.Vector3(i * 2.3, j * 3.7, 0).sub(this.offSet), .1);
       }
     }
   }
 
-  //actual game
-  //still nothing
-
 }
-
-const game = new Game(nb_card)
 
 camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 20
 
+let game = 0
 let haveRotate = []
 let cardUnder = []
 let explosions = []
@@ -285,13 +244,8 @@ const onMoveUp = (cardIndex) => {
 }
 
 
-const beforeSpread = (event) => {
-  socket.emit('ready', room, cb => {
-    if (cb) {
-      gameStart()
-    }
-  })
-}
+const beforeSpread = event => socket.emit('ready')
+
 
 
 const addListener = () => {
@@ -314,7 +268,7 @@ const gameStart = () => {
   window.removeEventListener('pointermove', updateMouse)
 
   setTimeout(() => {
-    addListener();
+    // addListener();
     startControls();
   }, 1200);
 
@@ -335,7 +289,7 @@ function tick() {
 
   for (let i = 0; i < explosions.length; i++) {
     const explosion = explosions[i]
-    if(explosion.clock.getElapsedTime() > 3){
+    if (explosion.clock.getElapsedTime() > 3) {
       explosion.remove()
       explosions.splice(0, 1)
       i--;
@@ -353,22 +307,47 @@ function tick() {
 }
 
 
+socket.on('receive-cards', (cards) => {
+  console.log(cards)
+  game = new Game(cards);
+})
+
+window.addEventListener('click', beforeSpread);
+window.addEventListener('pointermove', updateMouse);
+
+
 socket.on('connect', () => {
   console.log('success')
   socket.emit('join-room', name, room, cb => {
     console.log(cb)
   })
 })
+
 socket.on('update-room', dict => {
   console.log(dict)
 })
 
-socket.on('move-down', (cardIndex) => moveDown(cardIndex))
-socket.on('turn-card', (cardIndex) => game.allCard[cardIndex].rotate(0, Math.PI, -1))
-socket.on('pair-found', cardIndex => pairFound(cardIndex))
-socket.on('move-up', (cardIndex) => moveUp(cardIndex))
+
+socket.on('action', (action, cardIndex) => {
+  switch (action) {
+    case 'move-down':
+      moveDown(cardIndex)
+      break;
+    case 'turn-card':
+      game.allCard[cardIndex].rotate(0, Math.PI, -1)
+      break;
+    case 'pair-found':
+      pairFound(cardIndex)
+      break;
+    case 'move-up':
+      moveUp(cardIndex)
+      break;
+    case 'turnback-card':
+      game.allCard[cardIndex].rotate(Math.PI, 0, -1)
+  }
+})
+
 socket.on('next-player', () => addListener())
 socket.on('start-game', () => gameStart())
-socket.on('turnback-card', (cardIndex) => { game.allCard[cardIndex].rotate(Math.PI, 0, -1) })
 
 tick()
